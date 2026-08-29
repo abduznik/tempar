5c630413796a07af1197e6bdc70e71050c8f2a8b
abduznik <85239936+abduznik@users.noreply.github.com>
2026-08-29 21:02:57 +0300

    fix: match button_callback declaration in main.h

--- src/include/main.h
+++ src/include/main.h
@@ -1,7 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-void button_callback(int curr_but, int last_but, void *arg);
+void button_callback(u32 curr_but, u32 last_but, void *arg);
 void gamePause(SceUID thread_id);
 void gameResume(SceUID thread_id);
 int module_stop(int argc, char *argv[]);
